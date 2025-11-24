fb6a47ad9c9be0db848d2ba5b358990d86f15518
mixzhang150-code <mixzhang150@gmail.com>
2025-11-24 15:06:35 +0800

    refactor(auth): 重构认证控制器为异步函数并改进错误处理
    将回调函数改为async/await语法，添加try-catch块处理错误
    优化代码结构，提高可读性和维护性
    ```
    
    ```msg
    style(error): 改进错误页面样式和布局
    
    重写错误页面样式，采用现代化设计
    添加响应式布局和更好的用户体验

--- controller/authController.js
+++ controller/authController.js
@@ -1,28 +1,30 @@
-var bcrypt = require('bcrypt');
-const saltRounds = 10;
-var User = require('../models/user');
-var Portal = require('../models/portal');
+const bcrypt = require('bcrypt');
 const { validationResult } = require('express-validator/check');
+const User = require('../models/user');
+const Portal = require('../models/portal');
+
+const saltRounds = 10;
 
 /**
  * 用户登录页面
  * @param {Object} req - Express请求对象
  * @param {Object} res - Express响应对象
  */
-exports.login = function(req, res) {
-    var user = req.session.leveluser;
-    Portal.find()
-        .exec(function(err, portal) {
-            if(err) {
-                console.log(err);
-            }
-            res.render(req.portal.theme+"/cmslogin", {
-                user: user,
-                portal: portal[0],
-                title: "用户登陆",
-                info: req.flash('info')
-            })
-        })
+exports.login = async (req, res) => {
+    try {
+        const user = req.session.leveluser;
+        const portal = await Portal.find().exec();
+        
+        res.render(req.portal.theme + "/cmslogin", {
+            user,
+            portal: portal[0],
+            title: "用户登陆",
+            info: req.flash('info')
+        });
+    } catch (error) {
+        console.error('获取登录页面失败:', error);
+        res.status(500).send('服务器内部错误');
+    }
 }
 
 /**
@@ -30,18 +32,19 @@ exports.login = function(req, res) {
  * @param {Object} req - Express请求对象
  * @param {Object} res - Express响应对象
  */
-exports.reg = function(req, res) {
-    Portal.find()
-        .exec(function(err, portal) {
-            if(err) {
-                console.log(err);
-            }
-            res.render(req.portal.theme+'/cmsreg', {
-                portal: portal[0],
-                title: '用户注册',
-                info: req.flash('info')
-            })
-        })
+exports.reg = async (req, res) => {
+    try {
+        const portal = await Portal.find().exec();
+        
+        res.render(req.portal.theme + '/cmsreg', {
+            portal: portal[0],
+            title: '用户注册',
+            info: req.flash('info')
+        });
+    } catch (error) {
+        console.error('获取注册页面失败:', error);
+        res.status(500).send('服务器内部错误');
+    }
 }
 
 /**
@@ -49,56 +52,50 @@ exports.reg = function(req, res) {
  * @param {Object} req - Express请求对象
  * @param {Object} res - Express响应对象
  */
-exports.postreg = function(req, res) {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(422).json({
-            errors: errors.array()
-        });
-    }
-    var username = req.body.username;
-    var email = req.body.email;
-    // 使用bcrypt加密密码
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-        if (err) {
-            console.error('密码加密失败:', err);
-            res.redirect('/register');
-            return;
+exports.postreg = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array()
+            });
         }
-        var newuserobj = {
-            username: username,
-            email: email,
+        
+        const { username, email, password } = req.body;
+        
+        // 使用bcrypt加密密码
+        const hash = await bcrypt.hash(password, saltRounds);
+        
+        const newuserobj = {
+            username,
+            email,
             password: hash
+        };
+        
+        // 检查用户名是否已存在
+        const existingUser = await User.findOne({ username }).exec();
+        if (existingUser) {
+            req.flash('info', '此用户名已经被注册');
+            return res.redirect('/register');
         }
-    User.findOne({username: username})
-        .exec(function(err,user) {
-            if(err) {
-                console.log(err);
-            }
-            if(user) {
-                req.flash('info', '此用户名已经被注册');
-                return res.redirect('/register');
-            }
-            User.findOne({email: email})
-                .exec(function(err, user) {
-                    if(err) {
-                        console.log(err);
-                    }
-                    if(user) {
-                        req.flash('info', '此邮箱已经被注册');
-                        return res.redirect("/register");
-                    }
-                    var newuser = new User(newuserobj);
-                    newuser.save(function(err,user) {
-                        if(err) {
-                            console.log(err);
-                        }
-                        req.session.leveluser = user.username;
-                        res.redirect('/');
-                    });
-                })
-        });
-    });
+        
+        // 检查邮箱是否已存在
+        const existingEmail = await User.findOne({ email }).exec();
+        if (existingEmail) {
+            req.flash('info', '此邮箱已经被注册');
+            return res.redirect("/register");
+        }
+        
+        // 创建新用户
+        const newuser = new User(newuserobj);
+        const savedUser = await newuser.save();
+        
+        req.session.leveluser = savedUser.username;
+        res.redirect('/');
+    } catch (error) {
+        console.error('注册失败:', error);
+        res.redirect('/register');
+    }
 }
 
 /**
@@ -106,33 +103,38 @@ exports.postreg = function(req, res) {
  * @param {Object} req - Express请求对象
  * @param {Object} res - Express响应对象
  */
-exports.postlogin = function(req, res) {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(422).json({
-            errors: errors.array()
-        });
-    }
-    var email = req.body.email;
-    User.findOne({email:email})
-        .exec(function(err,user){
-            if(err) {
-                console.log(err);
-            }
-            if(!user) {
-                req.flash('info','对不起，邮箱或密码错误');
-                return res.redirect("/login");
-            }
-            // 使用bcrypt验证密码
-            bcrypt.compare(req.body.password, user.password, function(err, result) {
-                if (err || !result) {
-                    req.flash('info','对不起，邮箱或密码错误');
-                    return res.redirect("/login");
-                }
-                req.session.leveluser = user.username;
-                res.redirect("/");
+exports.postlogin = async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({
+                errors: errors.array()
             });
-        });
+        }
+        
+        const { email, password } = req.body;
+        
+        // 查找用户
+        const user = await User.findOne({ email }).exec();
+        if (!user) {
+            req.flash('info', '对不起，邮箱或密码错误');
+            return res.redirect("/login");
+        }
+        
+        // 使用bcrypt验证密码
+        const result = await bcrypt.compare(password, user.password);
+        if (!result) {
+            req.flash('info', '对不起，邮箱或密码错误');
+            return res.redirect("/login");
+        }
+        
+        req.session.leveluser = user.username;
+        res.redirect("/");
+    } catch (error) {
+        console.error('登录失败:', error);
+        req.flash('info', '登录时发生错误');
+        res.redirect("/login");
+    }
 }
 
 /**
@@ -140,7 +142,7 @@ exports.postlogin = function(req, res) {
  * @param {Object} req - Express请求对象
  * @param {Object} res - Express响应对象
  */
-exports.logout = function(req, res) {
+exports.logout = (req, res) => {
     req.session.leveluser = null;
     res.redirect("/");
 }
